f8641d504ffbedbc29ea892dbd3ab6e9dbd7365f
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2022-02-09 20:52:12 +0100

    Init

---
+++ Vjezbe/API/FIT_Api_Examples/Modul2/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FIT_Api_Examples.Data;
+using FIT_Api_Examples.Helper;
+using FIT_Api_Examples.Helper.AutentifikacijaAutorizacija;
+using FIT_Api_Examples.Modul0_Autentifikacija.Models;
+using FIT_Api_Examples.Modul2.Models;
+using FIT_Api_Examples.Modul2.ViewModels;
+using FIT_Api_Examples.Modul3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIT_Api_Examples.Modul2.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedList<Student>> GetAllPaged(string ime_prezime, int items_per_page, int page_number = 1)
+        {
+
+            var data = _dbContext.Student
+                .Include(s => s.opstina_rodjenja.drzava)
+                .Where(x => ime_prezime == null || (x.ime + " " + x.prezime).StartsWith(ime_prezime) || (x.prezime + " " + x.ime).StartsWith(ime_prezime)).OrderByDescending(s => s.prezime).ThenByDescending(s => s.ime)
+                .AsQueryable();
+            return PagedList<Student>.Create(data, page_number, items_per_page);
+        }
+
+
+    }
+}
